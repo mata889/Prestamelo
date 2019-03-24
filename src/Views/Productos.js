504390622723d54145513bb62a36faf235467bff
Diego <dam_900@hotmail.com>
2019-03-23 19:18:57 -0600

    en los productos ya no sale el mismo producto del usuario

--- src/Views/Productos.js
+++ src/Views/Productos.js
@@ -1,71 +1,74 @@
 import React, { Component } from 'react';
-import {JuegosP} from './JuegosP.json';
+import { JuegosP } from './JuegosP.json';
 import '../App.css';
-import {Container} from 'reactstrap';
+import { Container } from 'reactstrap';
 import firebase from 'firebase'
 import DisplayProduct from './DisplayProduct'
 
 class Ventas extends Component {
-    constructor(){
-        super();
-        this.state = {JuegosP, Temp: []}
-    }
+  constructor() {
+    super();
+    this.state = { JuegosP, Temp: [] }
+  }
 
-    componentWillMount = () => {
-        const nameRef = firebase.database().ref('productos')
-        nameRef.on('value',(snapshot) => {
-          var scores = snapshot.val()
-          var keys = Object.keys(scores)
-          var arreglo = []
-          for (var i=0; i<keys.length;i++){
-            var k = keys[i]
-            var a = scores[k].precio
-            var b =  scores[k].descripcion
-            var c = scores[k].nombre
-            var d = scores[k].foto
-            var arr = {
-              "precio": a,
-              "descripcion": b,
-              "nombre": c,
-              "foto": d,
-              "id": k,
-              "uid_propietario": scores[k].uid,  
-              "propietario": scores[k].correo    
-            }   
-            arreglo.push(arr)
+  componentWillMount = () => {
+    const nameRef = firebase.database().ref('productos')
+    nameRef.on('value', (snapshot) => {
+      var scores = snapshot.val()
+      var keys = Object.keys(scores)
+      var arreglo = []
+      for (var i = 0; i < keys.length; i++) {
+        var k = keys[i]
+        if (firebase.auth().currentUser.uid !== scores[k].uid) {
+          console.log(firebase.auth().currentUser.displayName)
+          var a = scores[k].precio
+          var b = scores[k].descripcion
+          var c = scores[k].nombre
+          var d = scores[k].foto
+          var arr = {
+            "precio": a,
+            "descripcion": b,
+            "nombre": c,
+            "foto": d,
+            "id": k,
+            "uid_propietario": scores[k].uid,
+            "propietario": scores[k].correo
           }
-          this.setState({Temp: arreglo})
-        })
+          arreglo.push(arr)
+        }
       }
-  
-      componentDidMount = () => {
-        firebase.auth().onAuthStateChanged(user => {
-          this.setState({nombre: user.displayName})
-          this.setState({imagen: user.photoURL})
-          this.setState({data: user.providerData})
-          this.setState({UID: user.uid})
-        })
-      }
+      this.setState({ Temp: arreglo })
+    })
+  }
 
-    render(){
-        const JuegosP = this.state.Temp.map((JuegosP,i) => {
-            return(
-                <div className="col-md-4">
-                  <div className="card mt-4">
-                    <DisplayProduct nombre={JuegosP.nombre} descripcion={JuegosP.descripcion} precio={JuegosP.precio} id={JuegosP.id} uid_propietario={JuegosP.uid_propietario} propietario={JuegosP.propietario}></DisplayProduct>
-                  </div>               
-                </div>
-            )
-        })
-        
-        return(
-            <Container>
-                <div className="row mt-4">
-                    {JuegosP}
-                </div>     
-            </Container>      
-        )
-    }      
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ nombre: user.displayName })
+      this.setState({ imagen: user.photoURL })
+      this.setState({ data: user.providerData })
+      this.setState({ UID: user.uid })
+    })
+  }
+
+  render() {
+    const JuegosP = this.state.Temp.map((JuegosP, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <DisplayProduct nombre={JuegosP.nombre} descripcion={JuegosP.descripcion} precio={JuegosP.precio} id={JuegosP.id} uid_propietario={JuegosP.uid_propietario} propietario={JuegosP.propietario}></DisplayProduct>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <Container>
+        <div className="row mt-4">
+          {JuegosP}
+        </div>
+      </Container>
+    )
+  }
 }
 export default Ventas;
 
--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -1,100 +1,101 @@
 import React from 'react';
-import { Card,  CardTitle,  Container, Row, Col,CardImg } from 'reactstrap';
-import {JuegosP} from './JuegosP.json'
+import { Card, CardTitle, Container, Row, Col, CardImg } from 'reactstrap';
+import { JuegosP } from './JuegosP.json'
 import Display from './Display'
 import firebase from 'firebase'
 import Agregar from './Agrega'
 
-class Profile extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = { 
-          JuegosP,
-          Tempo: [],
-          nombre: null,
-          imagen: null,
-          data: null,
-          UID: null
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      JuegosP,
+      Tempo: [],
+      nombre: null,
+      imagen: null,
+      data: null,
+      UID: null
+    }
+  }
+
+  componentWillMount = () => {
+    const nameRef = firebase.database().ref('productos')
+    nameRef.on('value', (snapshot) => {
+      var scores = snapshot.val()
+      var keys = Object.keys(scores)
+      var arreglo = []
+      for (var i = 0; i < keys.length; i++) {
+        var k = keys[i]
+        var a = scores[k].precio
+        var b = scores[k].descripcion
+        var c = scores[k].nombre
+        var d = scores[k].foto
+        var e = scores[k].uid
+        var arr = {
+          "precio": a,
+          "descripcion": b,
+          "nombre": c,
+          "foto": d,
+          "uid": e
         }
-    }
+        arreglo.push(arr)
+      }
+      this.setState({ Tempo: arreglo })
+    })
+  }
 
-    componentWillMount = () => {
-      const nameRef = firebase.database().ref('productos')
-      nameRef.on('value',(snapshot) => {
-        var scores = snapshot.val()
-        var keys = Object.keys(scores)
-        var arreglo = []
-        for (var i=0; i<keys.length;i++){
-          var k = keys[i]
-          var a = scores[k].precio
-          var b =  scores[k].descripcion
-          var c = scores[k].nombre
-          var d = scores[k].foto
-          var e = scores[k].uid
-          var arr = {
-            "precio": a,
-            "descripcion": b,
-            "nombre": c,
-            "foto": d,
-            "uid": e
-          }   
-          arreglo.push(arr)
-        }
-        this.setState({Tempo: arreglo})
-      })
-    }
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ nombre: user.displayName })
+      this.setState({ imagen: user.photoURL })
+      this.setState({ data: user.providerData })
+      this.setState({ UID: user.uid })
+    })
+  }
 
-    componentDidMount = () => {
-      firebase.auth().onAuthStateChanged(user => {
-        this.setState({nombre: user.displayName})
-        this.setState({imagen: user.photoURL})
-        this.setState({data: user.providerData})
-        this.setState({UID: user.uid})
-      })
-    }
 
-    render(){
-        const JuegosP = this.state.Tempo.map((JuegosP,i) => {       
-            return (
-              JuegosP.uid === this.state.UID ?
-              <div className="row mt-4">   
-                <Display precio={JuegosP.precio} description={JuegosP.descripcion} name={JuegosP.nombre} imagen={JuegosP.foto}></Display>
-              </div>
-              : ""
-            )
-          })
-          
-        return (
-            <div >
+  render() {
+    const JuegosP = this.state.Tempo.map((JuegosP, i) => {
+      return (
+        JuegosP.uid === this.state.UID ?
+          <div className="row mt-4">
+            <Display precio={JuegosP.precio} description={JuegosP.descripcion} name={JuegosP.nombre} imagen={JuegosP.foto}></Display>
+          </div>
+          : ""
+      )
+    })
+
+    return (
+      <div >
+        <Container>
+          <Row>
+            <Col xs="auto" sm="4" >
               <Container>
-                <Row>
-                    <Col xs="auto" sm="4" >
-                        <Container>
-                        <div className="row mt-4">        
-                            <Card body>
-                                <CardImg src={this.state.imagen}></CardImg>
-                                <br></br>
-                                <CardTitle><h3>{this.state.nombre}</h3></CardTitle>
-                               <Agregar></Agregar>
-                            </Card>
-                        </div>    
-                        </Container>
-                    </Col>
-                    <Col>
-                      {JuegosP}           
-                    </Col>
-                </Row>   
+                <div className="row mt-4">
+                  <Card body>
+                    <CardImg src={this.state.imagen}></CardImg>
+                    <br></br>
+                    <CardTitle><h3>{this.state.nombre}</h3></CardTitle>
+                    <Agregar></Agregar>
+                  </Card>
+                </div>
               </Container>
-                 
-            </div>
-          );
-    }
+            </Col>
+            <Col>
+              {JuegosP}
+            </Col>
+          </Row>
+        </Container>
+
+      </div>
+    );
+  }
 }
 
 export default Profile;
 
 /*Despues de <FileUpload></FileUpload>
                 <Switch>
-                 <Route path="/AddProducts" component={FileUpload}/> 
+                 <Route path="/AddProducts" component={FileUpload}/>
                 </Switch>
 */
